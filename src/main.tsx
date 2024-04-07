9997e5606b126b0b92037e45e15a3376777c76e4
edwardFlorez <edjflore@bancolombia.com.co>
2024-04-07 12:20:12 -0500

    add structure route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+
 import App from './App.tsx'
 import './index.css'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement: <NotFoundPage/>
+  },
+  {
+    path: '/login',
+    element: <LoginScreen/>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
