f82fcf58d4bb5bc5b0ac92d6c0c5339d1f511b61
edwardFlorez <edjflore@bancolombia.com.co>
2024-04-07 14:31:43 -0500

    msal structure is added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
+import { PublicClientApplication, EventType, EventMessage, AuthenticationResult } from '@azure/msal-browser'
+import { msalConfig } from './util/login-logic/auth-config.ts'
 
 
 import App from './App.tsx'
 import './index.css'
 import NotFoundPage from './pages/NotFoundPage.tsx'
+import { MsalProvider } from '@azure/msal-react'
 
 const router = createBrowserRouter([
   {
@@ -14,14 +16,28 @@ const router = createBrowserRouter([
     element: <App/>,
     errorElement: <NotFoundPage/>
   },
-  {
-    path: '/login',
-    element: <LoginScreen/>,
-  }
 ]);
+
+// MSAL should be instantiated otside of the component tree to prevent ir from being re-instantiated on re-renders
+const msalInstance = new PublicClientApplication(msalConfig);
+const accounts = msalInstance.getAllAccounts();
+
+if (accounts.length > 0) {
+  msalInstance.setActiveAccount(accounts[0]);
+}
+
+msalInstance.addEventCallback((event: EventMessage) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+      const payload = event.payload as AuthenticationResult;
+      const account = payload.account;
+      msalInstance.setActiveAccount(account);
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
     <RouterProvider router={router} />
+    </MsalProvider>
   </React.StrictMode>,
 )
---
+++ src/util/login-logic/auth-config.ts
@@ -0,0 +1,22 @@
+import { LogLevel } from "@azure/msal-browser";
+
+const url = 'http://localhost:56369';
+
+export const msalConfig = {
+    auth: {
+        clientId : '3e6ba0dd-bafc-4064-a810-f5916c17470e',
+        authority: 'https://login.microsoftonline.com/4dbe1e0a-b1f6-440f-9fde-f0593e5656ca',
+        redirectUri : url + '/',
+        postLogoutRedirectUri: url + '/login',
+        navigateToLoginRequestUrl : false,
+    },
+    cache : {
+        cacheLocation: 'sessionStorage',
+        storeAuthStateInCookie: false, 
+    },
+
+}
+
+export const loginScope = {
+    scopes: ['user.read']
+};
